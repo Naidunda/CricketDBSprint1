baba3321bdfef336ce7ab8ec3eebe9bbb784b97c
Joshua Naidoo <joshuanaidoo0408@gmail.com>
2023-07-23 18:06:55 +1200

    Completed css and html for player-profile webpage.

--- scripts/players.js
+++ scripts/players.js
@@ -3,13 +3,13 @@ function openTab(evt, tabName) {
     var i, tabcontent, tablinks;
   
     // Get all elements with class="tab-content-players" and hide them
-    tabcontent = document.getElementsByClassName("tab-content-players");
+    tabcontent = document.getElementsByClassName("tab-content");
     for (i = 0; i < tabcontent.length; i++) {
       tabcontent[i].style.display = "none";
     }
   
     // Get all elements with class="tab-links-player" and remove the class "active"
-    tablinks = document.getElementsByClassName("tab-links-players");
+    tablinks = document.getElementsByClassName("tab-links");
     for (i = 0; i < tablinks.length; i++) {
       tablinks[i].className = tablinks[i].className.replace(" active", "");
     }
