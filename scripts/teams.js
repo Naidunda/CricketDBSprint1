029dbf4abf2ae9eb3af9d3997a77e2e4f3496ac9
Joshua Naidoo <joshuanaidoo0408@gmail.com>
2023-08-03 21:47:45 +1200

    Update teams.js

--- scripts/teams.js
+++ scripts/teams.js
@@ -17,8 +17,7 @@ function openTab(evt, tabName) {
     // Show the current tab, and add an "active" class to the button that opened the tab
     document.getElementById(tabName).style.display = "block";
     evt.currentTarget.className += " active";
-}
-
+} 
 
 function openTeamProfile() {
   window.location.href='team-profile.html';
